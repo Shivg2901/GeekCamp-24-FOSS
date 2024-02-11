3891594085e63a310d51f3b154901ca400352504
Shivg2901 <guptashivansh374@gmail.com>
2024-02-11 22:05:52 +0530

    Task 3 Completed

--- Shivg2901.py
+++ Shivg2901.py
@@ -1,2 +1,6 @@
 1.Suits
 2.Aisa lagta hai ki unproductive nhi hua kuch bhi agar woh dekha toh
+3.Anshaniha Pal
+4.N
+5. I wrestled with Git and GitHub for some time, but thanks to the amazing folks in the FOSS Wing, I've got it all sorted out now.They were incredibly cooperative and helpful.
+
